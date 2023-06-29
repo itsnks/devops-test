2f4339a1ca5256195c9fe721706b8b717e31b9e0
Nikesh <eminem.nikesh@gmail.com>
2023-06-29 16:40:38 +0545

    informing partition table changes to OS

--- vagrant-basics/disk/resizeDisk.sh
+++ vagrant-basics/disk/resizeDisk.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ROOT_DISK_DEVICE="/dev/sda"
-ROOT_DISK_DEVICE_PART="dev/sda1"
+ROOT_DISK_DEVICE_PART="dev/sda2"
 LV_PATH=`sudo lvdisplay -c  | sed -n 1p | awk -F ":" '{print $1;}'`
 FS_PATH=`df | sed -n 2p | awk '{print $1;}'`
-ROOT_FS_SIZE=`wr`
 echo "The root file system (/) has a size of $ROOT_FS_SIZE"
 ECHO "> iNCREASING DISK SIZE OF $ROOT_DISK_DEVICE to available maximum"
 sudo fdisk $ROOT_DISK_DEVICE <<EOF
 d
+2
 n
 p
-
+2
 
 
 no
@@ -22,5 +22,7 @@ w
 EOF
 sudo pvresize $ROOT_DISK_DEVICE_PART
 sudo lvextend -l +100%FREE $LV_PATH
-sudo xfs_growfs $FS_PATH    #for red hat based systems
+/usr/sbin/partprobe
+/usr/sbin/xfs_growfs -d $FS_PATH
+#sudo xfs_growfs $FS_PATH    #for red hat based systems
 #sudo resize2fs -p $FS_PATH #use this if using debian based system
